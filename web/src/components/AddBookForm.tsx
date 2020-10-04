96bd38509fd39c3e07b24ca927efb65981ed0671
Tomasz Palys <tomek201992@gmail.com>
2020-10-04 18:35:42 +0200

    Add button which adds more authors

--- web/src/components/AddBookForm.tsx
+++ web/src/components/AddBookForm.tsx
@@ -1,44 +1,69 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
+import colors from '../theme/colors';
 import Input from './Input';
 import Select from './Select';
 
 const AddBookForm: React.FC = () => {
     const [title, setTitle] = useState<string>();
     const [numberOfPages, setNumberOfPages] = useState<number>();
+    const [numberOfAuthors, setNumberOfAuthors] = useState(1);
     return (
         <div>
-            <Input 
-                label="Title" 
-                value={title} 
-                onChange={setTitle}
-            />
-            <Select 
-                label="Authors"
-                options={[{
-                    id: '1',
-                    value: 'author1'
-                }, {
-                    id: '2',
-                    value: 'author2'
-                }]} 
-                value={{
-                    id: '2',
-                    value: 'author2'
-                }} />
-            <Input 
-                label="Number of pages" 
-                type="number" 
-                value={`${numberOfPages}`}
-                onChange={pages => {
-                    try {
-                        setNumberOfPages(parseInt(pages));
-                    } catch (e) {
-                        console.log(e);
-                    }
-                }} 
-            />
+            <Container>
+                <Input
+                    label="Title"
+                    value={title}
+                    onChange={setTitle}
+                />
+            </Container>
+            <Container>
+                <label>Authors</label>
+                {[...Array(numberOfAuthors)].map((_, i) => (
+                    <AuthorContainer>
+                        <Select
+                            key={i}
+                            options={[{
+                                id: '1',
+                                value: 'author1'
+                            }, {
+                                id: '2',
+                                value: 'author2'
+                            }]}
+                            value={{
+                                id: '2',
+                                value: 'author2'
+                            }} />
+                    </AuthorContainer>))}
+                <button onClick={() => setNumberOfAuthors(i => i + 1)}>More</button>
+            </Container>
+            <Container>
+                <Input
+                    label="Number of pages"
+                    type="number"
+                    value={`${numberOfPages}`}
+                    onChange={pages => {
+                        try {
+                            setNumberOfPages(parseInt(pages));
+                        } catch (e) {
+                            console.log(e);
+                        }
+                    }}
+                />
+            </Container>
         </div>
     );
 }
+
+const Container = styled.div`
+    padding: 10px;
+    margin-bottom: 10px;
+    border: 1px solid ${colors.LIGHT_GRAY};
+    border-radius: 3px;
+`;
+
+const AuthorContainer = styled.div`
+    margin-bottom: 10px;    
+`;
 
 export default AddBookForm;
